cb8cf66d25c9047e44c14678c2e262280519497d
Ma Su Mon Hlaing <sumonhlaing1997@gmail.com>
2025-04-23 16:54:03 +0630

    set up frontend and backend for dental clinic

--- workshop/workshop-3/src/App.js
+++ workshop/workshop-3/src/App.js
@@ -1,11 +1,22 @@
+import Bio from "./Bio.js";
 import Gallery from "./Gallery.js";
+import PersonTodoList from "./Person.js";
 import Profile from "./Profile.js";
+import TodoList from "./Todo.js";
+import { getImageUrl } from './utils.js';
+
 
 export default function App() {
   return (
     <div>
       <Profile />
       <Gallery />
+      <TodoList />
+      <Bio  />
+      <PersonTodoList />
+      <Card>
+     
+    </Card>
     </div>
   );
 }
